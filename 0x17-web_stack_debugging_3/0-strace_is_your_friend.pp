7ca97afd5e9a267862377018c13b0efd722ec14d
stanjae <stanykhay29@gmail.com>
2024-05-13 06:16:14 +0100

    lol

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 
 exec {'replace':
   provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path     => 'usr/local/bin/:/bin/'
 }
